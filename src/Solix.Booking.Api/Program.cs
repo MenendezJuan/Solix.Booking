f3c415ac96056f7cb4e5bbf8dea591f4a329d0f0
MenendezJuan <jumenendez01@gmail.com>
2024-01-26 14:11:48 -0300

    Update

--- src/Solix.Booking.Api/Program.cs
+++ src/Solix.Booking.Api/Program.cs
@@ -1,13 +1,32 @@
-using Microsoft.EntityFrameworkCore;
-using Solix.Booking.Persistence.Database;
+using Solix.Booking.Application.Interfaces;
+using Solix.Booking.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<DatabaseService>(options=>
-options.UseSqlServer(builder.Configuration["SqlConnectionString"]));
+// Configuración de servicios
+builder.Services.AddDbContextSqlServer(builder.Configuration);
+builder.Services.AddServices(); // Llamando al método de extensión AddServices
 
 var app = builder.Build();
 
+app.MapPost("/createTest", async (IServiceProvider serviceProvider) =>
+{
+	// Resuelve el servicio en el ámbito del método
+	using var scope = serviceProvider.CreateScope();
+	var databaseService = scope.ServiceProvider.GetRequiredService<IDatabaseService>();
+
+	var entidad = new Solix.Booking.Domain.Entities.Usuarios.Usuario
+	{
+		Nombre = "Juan",
+		Apellido = "Menendez",
+		NombreUsuario = "Juan123",
+		Password = "CD#rDE"
+	};
+
+	await databaseService.usuarios.AddAsync(entidad);
+	await databaseService.SaveAsync();
+
+	return "Create OK";
+});
 
 app.Run();
-
---
+++ src/Solix.Booking.Persistence/ServiceCollectionExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Solix.Booking.Application.Interfaces;
+using Solix.Booking.Persistence.Database;
+
+namespace Solix.Booking.Persistence
+{
+	public static class ServiceCollectionExtension
+	{
+		public static IServiceCollection AddDbContextSqlServer(this IServiceCollection services, IConfiguration configuration) 
+		{
+			string connectionString = configuration.GetConnectionString("SqlConnectionString");
+			services.AddDbContext<DatabaseService>(options=> 
+			options.UseSqlServer(connectionString),ServiceLifetime.Transient);
+
+			return services;
+		}
+
+		public static IServiceCollection AddServices (this IServiceCollection services)
+		{
+			services.AddScoped<IDatabaseService, DatabaseService>();
+			return services;
+		}
+	}
+}
