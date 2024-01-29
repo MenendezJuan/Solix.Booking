f4e920567f0f9da158761c69ae893b0a24f2e748
MenendezJuan <jumenendez01@gmail.com>
2024-01-29 20:32:46 -0300

    Update

---
+++ src/Solix.Booking.Domain/Models/BaseResponseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Solix.Booking.Domain.Models
+{
+	public class BaseResponseModel
+	{
+		public int StatusCode { get; set; }
+		public bool Success { get; set; }
+		public string Message { get; set; }
+		//Puede ser object, pero lo hacemos dynamic para poder setearle cualquier tipo de dato y accederlo sin necesidad de instanciarlo o inicializarlo
+		public dynamic data { get; set; }
+	}
+}
